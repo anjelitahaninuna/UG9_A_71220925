d266eeac7eaa276e8209d0aa062182bc8b474b41
anjelitahaninuna <118496692+anjelitahaninuna@users.noreply.github.com>
2022-11-18 10:35:30 +0700

    Add files via upload

---
+++ =UG9_A_1_71220925.py
@@ -0,0 +1,11 @@
+a=str(input("masukkan nama Anda:"))
+b=str(input("masukkan nama matakuliah:"))
+c=str(input("masukkan grup Anda:"))
+print("Hallo!",a*2)
+print("Anda tergabung dalam kelas",b,"pada grup", c)
+
+
+
+
+
+
---
+++ ug9_a_2_71220925.py
@@ -0,0 +1,8 @@
+SuhuF = int(input("suhu dalam Fahretheitr:"))
+FkeC= int((5/9)*(suhuF-32))
+print (suhuF,"derajat Fahrenheit dikonversi menjadi",FkeC,"derajat Celcius")
+
+#test 2.b
+suhuF= int(input("suhu dalam skala fahrenheit:"))
+FkeC= int((5/9)*(suhuF-32))
+print (suhuF."Derajat Fahrenheit dikonversi menjadi", FkeC."Derajat Calcius")
